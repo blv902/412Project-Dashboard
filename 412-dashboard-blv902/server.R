c7cd843ce3f58a4ff1f18ba5b1931de7842744d5
Benjamin Vong <benjamn902@gmail.com>
2022-02-27 01:22:44 -0800

    Added comments and created general structure of shiny dashboard. Finished webscraping and added userinput years to tab 1

--- 412-dashboard-blv902/server.R
+++ 412-dashboard-blv902/server.R
@@ -1,47 +1,99 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#facebook
-# https://www.sec.gov/edgar/browse/?CIK=1326801&owner=exclude
-#apple
-#https://www.sec.gov/edgar/browse/?CIK=320193&owner=exclude
-
+#Author: Benjamin Vong
+#title: SEC Dashboard
+#date: 
 library(shiny); library(dplyr); library(edgar); library(request); library(rvest); library(stringr); library(httr)
-#Example: https://www.sec.gov/Archives/edgar/data/1326801/000132680119000069/fb-09302019x10q.htm
 
-# Define server logic required to draw a histogram
+
+# Define server logic
 shinyServer(function(input, output) {
+  #Function will test user link with SEC
   get_web = function(link) {
+    #remove excess char in string and save into CIK
     CIK = substring(link, first = 39)
+    #isolate CIK from rest of string
     CIK = gsub("&owner=exclude", "", CIK)
+    #edits link to function in edgar search
     link = gsub("https://www.sec.gov/", "https://www.sec.gov/cgi-bin/", link)
     link = gsub("edgar/browse/", "browse-edgar", link)
+    #counts the numbers of char in CIK
     countCIK = str_count(CIK)
     fullCIK = CIK
+    #format CIK into 10 digit to be used in SEC website
     while (countCIK <= 9) {
       fullCIK = paste0("0", fullCIK)
       countCIK = countCIK + 1
     }
+    #combines the parts of the link together
     link = paste("https://www.sec.gov/cgi-bin/browse-edgar?CIK=", fullCIK, "&owner=exclude", sep = "")
     secURL <- link
-    agentURL <- GET(secURL, add_headers('user-agent' = 'SEC listing scraper'))
+    #sets the user agent to web scrap
+    agentURL <- GET(secURL, add_headers('user-agent' = 'blv902@uw.edu'))
+    #read the the html
     page <- read_html(agentURL)
+    #parse the html for the exact Name and SIC
     companyName = page %>% html_nodes(".companyName ") %>% html_text()
     companySIC = page %>% html_nodes(".identInfo acronym+ a") %>% html_text()
-    return(companyName)
+    #bundle the variables into an df
+    companyDetail = data.frame(companyName, companySIC, CIK, stringsAsFactors = FALSE)
+    #return name, SIC, and CIK
+    return(companyDetail)
   }
-  
+  #convert string userinput into intergers
+  get_Years = function() {
+    #counts the commas in the userinput and adds one
+    yearsCount = str_count(input$rawYears, ",") + 1
+    arrayCount = 0
+    #removes the commas
+    years = gsub(","," ",input$rawYears)
+    #makes an array based on amount of years
+    arrayYear = array(data = NA, dim = c(1, yearsCount))
+    #loop to add and covert years into the array
+    while(arrayCount <= yearsCount) {
+      arrayYear[arrayCount] = strtoi(word(years,arrayCount))
+      arrayCount = arrayCount + 1
+    }
+    #returns an array of years
+    return(arrayYear)
+  }
+  #uses converted userinput to get SEC filings
+  get_Form = function() {
+    #counts userinputted years
+    yearsCount = str_count(input$rawYears, ",") + 1
+    arrayCount = 0
+    #gets CIK from get_web array num 3
+    CIK = get_web(input$rawLink)[3]
+    #call get_years to get array year
+    year = get_Years()
+    #runs through the year array to get each year filing
+    while (arrayCount <= yearsCount) {
+      getFilings(CIK, '10-K', year[arrayCount], downl.permit = "n", useragent = 'blv902@uw.edu')
+      arrayCount = arrayCount + 1
+    }
+  }
+  #will update when user changes SEC link
   observeEvent(input$rawLink, {
     get_web(input$rawLink)
   })
-    
+  #will update when user changes years
+  observeEvent(input$rawYears, {
+    get_Years()
+  })
+  #will update when user start
+  observeEvent(input$rawStart, {
+    #signal to get form when checked
+    if (input$rawStart == TRUE) {
+      get_Form()
+    }
+  })
+  #display the company name and CIK so that user can double check
   output$outputLink <- renderText({
-    paste(get_web(input$rawLink))
+    companyName = get_web(input$rawLink)[1]
+    paste(companyName)
     
-    })
+  })
+  
+  output$outputBalance <- renderTable({
+    
+  })
 
 })
--- 412-dashboard-blv902/ui.R
+++ 412-dashboard-blv902/ui.R
@@ -1,11 +1,6 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#Author: Benjamin Vong
+#title: SEC Dashboard
+#date: 
 
 library(shiny) ; library(edgar) ; library(shinythemes)
 
@@ -16,14 +11,80 @@ shinyUI(fluidPage(
   #setup page structure and set title
   navbarPage(title = "SEC Filing Dashboard",
     #First tab bar with SEC input
-    sidebarPanel(
-      tags$h1("Input"),
-      textInput("rawLink", "Please enter SEC link", "https://www.sec.gov/edgar/browse/?CIK=320193&owner=exclude"),
+    tabPanel("Setup",
+             #Separates first tab into 4 section
+             fluidRow(
+               column(4,
+                      #Ask for user's email to report to SEC
+                      textInput("userEmail", "Please enter email (required by SEC)", "useragent@domain.com"),
+                      br(),
+                      #Ask if user wants 10-k or 10-Q
+                      paste('Please select type of form'),
+                      checkboxInput('raw10-k', '10-k', value = TRUE),
+                      checkboxInput('raw10-Q', '10-Q', value = FALSE),
+               ),
+               column(4,
+                      #Ask for the SEC link
+                      textInput("rawLink", "Please enter SEC link", "https://www.sec.gov/edgar/browse/?CIK=320193&owner=exclude"),
+                      br(),
+                      textInput("rawYears", "Please enter years (Example:2009,2010,2019,2021)", "2009,2010"),
+               ),
+               column(4,
+                      #Ask what quarter if 10-Q
+                      sliderInput("rawQuarter", "Select no. of quarter",1,4,1),
+                      h5("Quarter 1: January 1 to March 31"),
+                      h5("Quarter 2: April 1 to June 30"),
+                      h5("Quarter 3: July 1 to September 30"),
+                      h5("Quarter 4: October 1 to December 31"),
+               ),
+             ),
+    #Breaks into next part
+    hr(),
+    #Display the company the user selected
+    h1("Company"),
+    verbatimTextOutput("outputLink"),
+    paste('Press to start'),
+    checkboxInput('rawStart', 'Start?', value = FALSE),
+    
     ),
+    #Create drop down list
+    navbarMenu("Financial Tables",
+        #First tab bar
+        tabPanel("Balance Sheet",
+             mainPanel(
+               h1("Balance Sheet Table"),
+               tableOutput("outputBalance")
+             ),
+        ),
+        #Second tab bar
+        tabPanel("Cash Flow",
+             h1("Main")
+        ),
+        #Third tab bar
+        tabPanel("Income Statement",
+             h1("Main")
+        ),
         # Show user what is inputted
-        mainPanel(
-            h1("Company"),
-            verbatimTextOutput("outputLink")
-        )
+        
+    ),
+    navbarMenu("Graph",
+               #First tab bar
+               tabPanel("Balance Sheet",
+                        mainPanel(
+                          h1("Balance Sheet Table"),
+                          
+                        ),
+               ),
+               #Second tab bar
+               tabPanel("Cash Flow",
+                        h1("Main")
+               ),
+               #Third tab bar
+               tabPanel("Income Statement",
+                        h1("Main")
+               ),
+               # Show user what is inputted
+               
     )
+  )
 ))
